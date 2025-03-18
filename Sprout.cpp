18cdd40ddf7be495be696d99e338a9a60d04300b
lancechoe <122698341+lancechoe@users.noreply.github.com>
2025-03-18 12:59:49 +0900

    Add files via upload

---
+++ Sprout.cpp
@@ -0,0 +1,29 @@
+/*
+Problem
+Print a sprout as shown in the example below.
+
+Input
+There is no input.
+
+Output
+Print the sprout.
+
+         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "         ,r'\"7" << endl;
+    cout << "r`-_   ,'  ,/" << endl;
+    cout << " \\. \". L_r'" << endl;
+    cout << "   `~\\/" << endl;
+    cout << "      |" << endl;
+    cout << "      |" << endl;
+}
